4a88030f5d0828eb70e65ecb8ab1fffe2deccbed
g. nicholas d'andrea <nick@gnidan.org>
2022-12-01 22:19:46 -0500

    Initialize Sphinx project

---
+++ docs/source/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Ethereum Debugging Data Format'
+copyright = "2022, g. nicholas d'andrea <gnidan@consensys.net>, et al."
+author = "g. nicholas d'andrea <gnidan@consensys.net>, et al."
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/source/index.md
@@ -0,0 +1,8 @@
+# Ethereum Debugging Data Format
+
+```{toctree}
+:maxdepth: 2
+:caption: Contents
+
+./overview.md
+```
