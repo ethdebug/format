3654f227edb79a962e05962836beddcd653f5924
g. nicholas d'andrea <nick@gnidan.org>
2023-12-21 23:17:57 -0500

    Specify ethdebug/format/type/base
    ... to serve as a minimally-necessary schema for all type
    representations, to be extended by more-specific subschemas for known
    kinds of types.
    
    Specifically:
    
    - Define formal JSON Schema in YAML
    - Add "Type schemas" section to specification site
      - Include overview page to describe how type schemas are organized
      - Include placeholder page for to-be-written canonical type schema
    
    - Add page to Type schemas section to informally specify
      ethdebug/format/type/base
      - Introduce purpose of this schema and how it fits more broadly
      - Explain key concepts defined by this schema
      - Represent full base schema via interactive nested React components
      - Provide example (non-canonical) schema extensions for a few types

--- web/src/loadSchema.ts
+++ web/src/loadSchema.ts
@@ -1,4 +1,8 @@
+import typeBaseSchemaYaml from "../../schemas/type/base.schema.yaml";
+
+
 export const schemas = [
+  typeBaseSchemaYaml,
 ].map(schema => ({
   [schema.$id]: schema
 })).reduce((a, b) => ({ ...a, ...b }), {});
