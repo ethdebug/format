daaf6e8d9a0b3d8b1c08ca2b887fcd3dd3f55929
g. nicholas d'andrea <nick@gnidan.org>
2023-12-21 22:08:41 -0500

    Remove schema stubs

--- web/src/loadSchema.ts
+++ web/src/loadSchema.ts
@@ -1,7 +1,4 @@
-import typesSchemaYaml from "../../schemas/types.schema.yaml";
-
 export const schemas = [
-  typesSchemaYaml
 ].map(schema => ({
   [schema.$id]: schema
 })).reduce((a, b) => ({ ...a, ...b }), {});
