7b423530499ce058492cd095c0161edeaec488d6
g. nicholas d'andrea <nick@gnidan.org>
2023-12-21 22:02:52 -0500

    [wip] ethdebug/format/operation*

--- web/src/loadSchema.ts
+++ web/src/loadSchema.ts
@@ -1,9 +1,14 @@
 import typeSchemaYaml from "../../schemas/type.schema.yaml";
 import typeBaseSchemaYaml from "../../schemas/type/base.schema.yaml";
+import operationSchemaYaml from "../../schemas/operation.schema.yaml";
+import operationBaseSchemaYaml from "../../schemas/operation/base.schema.yaml";
+
 
 export const schemas = [
   typeSchemaYaml,
-  typeBaseSchemaYaml
+  typeBaseSchemaYaml,
+  operationSchemaYaml,
+  operationBaseSchemaYaml
 ].map(schema => ({
   [schema.$id]: schema
 })).reduce((a, b) => ({ ...a, ...b }), {});
