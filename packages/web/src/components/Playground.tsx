fc0a42d1a51c1345481827044815da0b67fb3e6d
rahul <raxhvl@users.noreply.github.com>
2024-02-13 13:37:44 +0530

    feat: JSON source mapping

--- packages/web/src/components/Playground.tsx
+++ packages/web/src/components/Playground.tsx
@@ -3,7 +3,7 @@ import Editor, { useMonaco } from "@monaco-editor/react";
 import { editor } from "monaco-editor";
 import { useEffect, useRef, useState } from "react";
 import { useColorMode } from "@docusaurus/theme-common";
-import "react-toastify/dist/ReactToastify.css";
+import JSONSourceMap from "@mischnic/json-sourcemap";
 
 // To use Ajv with the support of all JSON Schema draft-2019-09/2020-12
 // features you need to use a different export:
@@ -14,8 +14,24 @@ export interface PlaygroundProps {
   schema: SchemaById;
 }
 
+/* Source map types */
+type SourceMap = { data: any; pointers: Record<string, Pointer> };
+
+type Pointer = {
+  value: Position;
+  valueEnd: Position;
+  key?: Position;
+  keyEnd?: Position;
+};
+
+type Position = {
+  line: number;
+  column: number;
+  pos: number;
+};
+
 /* The EthDebug Playground: An interactive component for developers */
-/* and tinkerers to build and test EthDebug schemas.               */
+/* and tinkerers to build and test EthDebug schemas.                */
 export default function Playground(props: PlaygroundProps): JSX.Element {
   const { schema } = describeSchema(props);
   const { colorMode } = useColorMode();
@@ -29,8 +45,20 @@ export default function Playground(props: PlaygroundProps): JSX.Element {
   // Ref to hold the Monaco editor instance
   const monaco = useMonaco();
 
+  // Tab width
+  const TAB_WIDTH = 2;
+
   // Compile all schemas to Ajv instance
-  const ajv = new Ajv({ schemas: Object.values(schemas), strict: false });
+  const ajv = new Ajv({
+    schemas: Object.values(schemas),
+    strict: false,
+    allErrors: true,
+    verbose: true,
+    messages: true,
+    code: {
+      source: true,
+    },
+  });
 
   // State to hold editor input
   const [editorInput, setEditorInput] = useState(exampleSchema);
@@ -50,116 +78,81 @@ export default function Playground(props: PlaygroundProps): JSX.Element {
   }
 
   /**
-   * Shows validation error in the Monaco editor
-   * If the key is found, it is highlighted as error.
-   * Otherwise, the whole JSON is highlighted.
-   * @param {string | undefined} key - The key to highlight
-   * @param {string} message - The error message
-   */
-  function showValidationError(key: string | undefined, message: string) {
-    const model = editorRef.current?.getModel();
-    if (!model) return showError("Unable to validate schema");
-
-    const lines = editorInput.split("\n");
-    let keyFound = false;
-
-    for (let i = 1; i <= lines.length; i++) {
-      const line = lines[i - 1];
-      const keyIndex = line.indexOf(`"${key}"`);
-
-      if (keyIndex !== -1) {
-        // Found the key in this line
-        monaco?.editor.setModelMarkers(model, "EthDebug", [
-          {
-            startLineNumber: i,
-            startColumn: 1,
-            endColumn: line.length,
-            endLineNumber: i,
-            message,
-            severity: monaco.MarkerSeverity.Error,
-          },
-        ]);
-        keyFound = true;
-      }
-    }
-
-    if (!key || !keyFound) {
-      monaco?.editor.setModelMarkers(model, "EthDebug", [
-        {
-          startLineNumber: 1,
-          startColumn: 1,
-          endColumn: 10,
-          endLineNumber: lines.length,
-          message,
-          severity: monaco.MarkerSeverity.Error,
-        },
-      ]);
-    }
-  }
-
-  /**
-   * Clears validation errors in the Monaco editor
-   */
-  function clearValidationErrors() {
-    const model = editorRef.current?.getModel();
-    if (!model) return showError("Unable to validate schema");
-    monaco?.editor.setModelMarkers(model, "EthDebug", []);
-  }
-
-  /**
    * Validates the schema using Ajv and displays errors in the Monaco editor
    */
   function validateSchema() {
     const validate = ajv.getSchema(props.schema.id);
     if (!validate) return showError("Unable to validate schema");
-    const isValid = validate(getParsedEditorInput());
+    const sourceMap = getParsedEditorInput();
+    const isValid = validate(sourceMap.data);
 
-    clearValidationErrors();
+    showValidationErrors(validate.errors, sourceMap);
+  }
 
-    validate.errors?.forEach((error: ErrorObject) => {
-      let key = error.instancePath.split("/").pop();
-      if (!isValid && error.message) {
-        showValidationError(key, getFormattedErrorMessage(error));
+  /**
+   * Shows validation error in the Monaco editor
+   * @param {ErrorObject[] | null | undefined} errors - Validation errors if any
+   * @param {string} sourceMap - The source map of the editor input
+   */
+  function showValidationErrors(
+    errors: ErrorObject[] | null | undefined,
+    sourceMap: SourceMap
+  ) {
+    const model = editorRef.current?.getModel();
+    if (!model || !monaco) return showError("Unable to validate schema");
+    let markers = [];
+    if (errors) {
+      for (const [_, error] of Object.entries(errors)) {
+        let node = sourceMap.pointers[error.instancePath];
+        const message = error.message;
+
+        if (!node || !message) continue;
+
+        markers.push({
+          startLineNumber: node.value.line + 1,
+          startColumn: node.value.column + 1,
+          endColumn: node.valueEnd.line + 1,
+          endLineNumber: node.valueEnd.column + 1,
+          message,
+          severity: monaco.MarkerSeverity.Error,
+        });
+
+        if (node.key && node.keyEnd) {
+          markers.push({
+            startLineNumber: node.key.line + 1,
+            startColumn: node.key.column + 1,
+            endColumn: node.keyEnd.line + 1,
+            endLineNumber: node.keyEnd.column + 1,
+            message,
+            severity: monaco.MarkerSeverity.Error,
+          });
+        }
       }
-    });
+    }
+    monaco.editor.setModelMarkers(model, "EthDebug", markers);
   }
 
   /**
    * Parses the editor input into a JSON object
    * @returns {Object} - Parsed JSON object
    */
-  function getParsedEditorInput() {
+  function getParsedEditorInput(): SourceMap {
     try {
-      return JSON.parse(editorInput);
+      return JSONSourceMap.parse(editorInput, undefined, {
+        tabWidth: TAB_WIDTH,
+      });
     } catch {
-      showError("Schema is NOT a valid JSON.");
+      return { data: "", pointers: {} };
     }
-  }
-
-  /**
-   * Formats an error message based on the Ajv ErrorObject.
-   * If instancePath exists, it is included in the message.
-   * If params.allowedValue exists, an additional message about the allowed value is included.
-   *
-   * @param {ErrorObject} error - The Ajv ErrorObject containing details about the validation error.
-   * @returns {string} - The formatted error message.
-   */
-  function getFormattedErrorMessage(error: ErrorObject): string {
-    const instancePathMessage = error.instancePath
-      ? `${error.instancePath}: `
-      : "";
-    const allowedValueMessage =
-      error.params && error.params.allowedValue
-        ? `Allowed value: ${error.params.allowedValue}.`
-        : "";
-    return `${instancePathMessage}${error.message}. ${allowedValueMessage}`;
   }
 
   /**
    * Displays an error message in the console
    * @param {string} error - The error message
    */
-  function showError(error: string) {}
+  function showError(error: string) {
+    console.error(error);
+  }
 
   /**
    * Handles editor value change event
@@ -176,7 +169,7 @@ export default function Playground(props: PlaygroundProps): JSX.Element {
         height="50vh"
         language="json"
         theme={colorMode == "dark" ? "vs-dark" : "vs-light"}
-        defaultValue={JSON.stringify(exampleSchema, undefined, 2)}
+        defaultValue={JSON.stringify(exampleSchema, undefined, TAB_WIDTH)}
         onChange={handleEditorChange}
         onMount={handleEditorDidMount}
       />
