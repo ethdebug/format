9471413aa0e8c5130f09da1682dfbcb633838413
g. nicholas d'andrea <nick@gnidan.org>
2025-02-03 00:11:35 -0500

    Define remark program instruction context

--- packages/web/src/schemas.ts
+++ packages/web/src/schemas.ts
@@ -215,7 +215,7 @@ const programSchemaIndex: SchemaIndex = {
   },
 
   ...(
-    ["code", "variables"].map(name => ({
+    ["code", "variables", "remark"].map(name => ({
       [`schema:ethdebug/format/program/context/${name}`]: {
         href: `/spec/program/context/${name}`
       }
