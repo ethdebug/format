2126ab7df1da33493be92cca57ec4afb4d6595dc
g. nicholas d'andrea <nick@gnidan.org>
2025-02-05 00:15:46 -0500

    Define @ethdebug/materials package

---
+++ packages/materials/src/data.test.ts
@@ -0,0 +1,38 @@
+/// <reference path="../../../jest.d.ts" />
+import { expect, describe, it } from "@jest/globals";
+
+import { describeSchema } from "@ethdebug/format";
+
+import { Data } from "./index.js";
+
+describe("type guards", () => {
+  const schemaGuards = [
+    {
+      schema: {
+        id: "schema:ethdebug/format/data/value"
+      },
+      guard: Data.isValue
+    },
+    {
+      schema: {
+        id: "schema:ethdebug/format/data/unsigned"
+      },
+      guard: Data.isUnsigned
+    },
+    {
+      schema: {
+        id: "schema:ethdebug/format/data/hex"
+      },
+      guard: Data.isHex
+    },
+  ] as const;
+
+  it.each(schemaGuards)("matches its examples", ({
+    guard,
+    ...describeSchemaOptions
+  }) => {
+    const { schema: { examples = [] } } = describeSchema(describeSchemaOptions);
+
+    expect(guard).toSatisfyAll(examples);
+  });
+});
