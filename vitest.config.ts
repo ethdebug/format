77e1e0fa8127fcafb668e894626f8968d96ef2e3
g. nicholas d'andrea <nick@gnidan.org>
2025-02-12 00:52:48 -0500

    Add coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    workspace: ["packages/*"]
+    workspace: ["packages/*"],
+    coverage: {
+      enabled: true,
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "./bin/**",
+        "./packages/web/**",
+        "./packages/format/bin/**",
+        "./packages/pointers/bin/**",
+        "**/dist/**"
+      ],
+    }
   }
 });
